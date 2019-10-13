639b83018ba13dbad76a2431b12994a2ef02037f
Rachid Ait Slimane <rachid.ait-slimane@ens.uvsq.fr>
2019-10-13 19:16:06 +0200

    Cree la fonction addotion() dans le fichier programme.c la tester dans la fonction main .

--- programme.c
+++ programme.c
@@ -5,8 +5,15 @@ void hello()
 	printf("Hello word !\n");
 }
 
+int addition(int a,int b)
+{
+	return a+b;
+}
+
 int main()
 {
 	hello();
+	int a=5,b=6;
+	printf("le resultat est : %d\n",addition(a,b));
 	return 0;
 }
